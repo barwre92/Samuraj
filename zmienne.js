68133b3a1ba851acd93d8c6e811ffa8546403db1
Bartman <bar.wre@gmail.com>
2024-01-28 18:56:09 +0100

    change names and add one space

--- zmienne.js
+++ zmienne.js
@@ -1,10 +1,11 @@
-let szuflada1 = "Ala";
+let szuflada1 = "Beata";
 let szuflada2 = 20 + 33;
 console.log(szuflada1);
 console.log(szuflada2);
 
-szuflada1 = "Ania";
+szuflada1 = "Kasia";
 szuflada2 = 99 + 1;
+
 
 console.log(szuflada1);
 console.log(szuflada2);
